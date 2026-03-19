62eb2e57ddc99b96e664c5f0648a94f3b5b0a7f8
Barkha dhakad <dhakadbarkha25@gmail.com>
2026-03-19 16:30:07 +0000

    datatypes

---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountId =145769
+let accountEmail = "barkh@gamil.com"
+var accountPassword = 1335
+accountcity = "Jaipur"
+
+// var ka use nhi krna hai because  complier confuse ho jata hai
+
+// const me variable kki value change kr skte hai..
+
+accountEmail = "bittugmail.com"
+accountPassword ="213244"
+accountcity ="Benguluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity])
