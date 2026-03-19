9a98e344829cafe4def85315927daf5400eff0a8
Barkha dhakad <dhakadbarkha25@gmail.com>
2026-03-19 17:00:09 +0000

    data2

---
+++ 01_basic/datatype.js
@@ -0,0 +1,20 @@
+
+let age = 21
+let islogedIn = false
+let state = null
+
+
+console.log(age);
+// //number 2 to power 53
+// bigint
+// string => ""
+// boolean => false / true
+// null =>standalone Value
+// undefined
+//symbol =>unique
+
+//Object
+console.log(typeof "Abhi");
+console.log(typeof 134);
+console.log(typeof age);
+console.log(typeof undefined);
