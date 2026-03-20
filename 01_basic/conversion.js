59df18034b1f40399c07c9914bfd380e0cad01b2
Barkha dhakad <dhakadbarkha25@gmail.com>
2026-03-20 06:43:41 +0000

    convert

---
+++ 01_basic/conversion.js
@@ -0,0 +1,44 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+ let score =  "3abc"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let name  = "Barkha"
+// let valueInstring = Number(name)
+// console.log(typeof valueInstring);
+// console.log(valueInstring);
+
+// let value = null
+// let Value = Number(null)
+// console.log(typeof Value);
+// console.log(Value);
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = "Barkha"
+// let isLoggedIn = ""
+// let Value = Boolean(isLoggedIn)
+// console.log(typeof Value);
+// console.log(Value);
+/*boolean me 
+" " => false
+"barkha" =>true
+1 =>true
+0 =>false */
+
+/* Number m convert krne pr */
+//"33" =>33
+//"33abc" =>NaN
+// true =>1; false =>0
+
+/*String me */
+let Set = 33
+let stringNumber = String(Set)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
