2fb5831ab6da9da87dd9cfca4c367ce9e75831f0
Barkha dhakad <dhakadbarkha25@gmail.com>
2026-03-20 07:31:41 +0000

    readability

---
+++ 01_basic/stringcon.js
@@ -0,0 +1,43 @@
+let value = 3
+let nagValue = -value
+// console.log(nagValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+//Strinng Add krna 
+/*let str1 = "AB";
+let str2 = "CD";
+let str3 = str1 +str2;
+console.log(str3) */
+
+// Ase mt likhna code readable nhi lgega nhi to
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3"); // 33 Ayega..
+
+console.log(12%5  * 5 + 1); // not readable code
+console.log((12%5)  * 5 + 1);//readable code
+
+console.log(true);
+console.log(+true); // not readable code
+
+
+console.log(+""); // not readability code
+
+let num,num2, num3
+num = num2 = num3 = 2*2  // not readability code
+
+let game = 100
+game++;//Or
+++game;
+
+console.log(game);
+// prifix Or postfix  pdana hai..
